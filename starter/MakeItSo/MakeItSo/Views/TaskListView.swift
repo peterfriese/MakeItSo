4edb2e207077f93c34aaf2b598b6fe3e6c2df8ac
Peter Friese <peter@peterfriese.de>
2020-01-17 16:00:45 +0100

    🚀 Add starter project and assets

---
+++ starter/MakeItSo/MakeItSo/Views/TaskListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MakeItSo
+//
+//  Created by Peter Friese on 10/01/2020.
+//  Copyright © 2020 Google LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    TaskListView()
+  }
+}
