dc67299cab9d8922f92517abd630a0d0f891593c
Peter Friese <peter@peterfriese.de>
2020-01-17 16:25:22 +0100

    ✨ Implement basic version of the UI

--- final/MakeItSo/MakeItSo/Views/TaskListView.swift
+++ final/MakeItSo/MakeItSo/Views/TaskListView.swift
@@ -9,12 +9,40 @@
 import SwiftUI
 
 struct TaskListView: View {
+  var tasks: [Task] = testDataTasks
+  
   var body: some View {
-    Text("Hello, World!")
+    NavigationView {
+      VStack(alignment: .leading) {
+        List {
+          ForEach (self.tasks) { task in
+            HStack {
+              Image(systemName: "circle")
+              .resizable()
+              .frame(width: 20, height: 20)
+              Text(task.title)
+            }
+          }
+          .onDelete { indexSet in
+          }
+        }
+        Button(action: {}) {
+          HStack {
+            Image(systemName: "plus.circle.fill")
+              .resizable()
+              .frame(width: 20, height: 20)
+            Text("New Task")
+          }
+        }
+        .padding()
+        .accentColor(Color(UIColor.systemRed))
+      }
+      .navigationBarTitle("Tasks")
+    }
   }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct TaskListView_Previews: PreviewProvider {
   static var previews: some View {
     TaskListView()
   }
