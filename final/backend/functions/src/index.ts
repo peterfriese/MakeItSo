5f8ad37c2e937eb004c985c7bca0505d28850470
Peter Friese <peter@peterfriese.de>
2020-10-30 18:10:12 +0100

    ♻️ Refactor to use transactions

--- final/backend/functions/src/index.ts
+++ final/backend/functions/src/index.ts
@@ -1,60 +1,69 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
-console.log(`Starting up MakeItSo Cloud Functions`)
+let db: FirebaseFirestore.Firestore
+const logger = functions.logger
+let initialized = false
 
-admin.initializeApp()
-const db = admin.firestore()
+function initialize() {
+  if (initialized === true) return
+  initialized = true
+  
+  logger.log(`Starting up MakeItSo Cloud Functions`)
+  admin.initializeApp()
+  db = admin.firestore()  
+}
 
-export const migrateTasks = functions.https.onCall(async (data, context) => {
-  console.log('Received data: %j', data)
 
-  const idToken = data.idToken
-
-  try {
-    console.log(`Verifying ID token ${idToken}`)
-    const decodedToken = await admin.auth().verifyIdToken(idToken)
-    if (context.auth) {
-      return performMigration(decodedToken, context.auth?.token)
-    }
-    else {
-      return {
-        error: "Context was empty"
-      }
-    }      
-  } 
-  catch (error) {
-    console.log(`Unable to verify ID token ${idToken}. Error: ${error}`)
-    return { error }
-  }
-})
-
-async function performMigration(previousUserToken: admin.auth.DecodedIdToken, currentUserToken: admin.auth.DecodedIdToken) {
+async function performMigration(
+  previousUserToken: admin.auth.DecodedIdToken, 
+  currentUserToken: admin.auth.DecodedIdToken
+  ) {
   const previousUserId = previousUserToken.uid
   const currentUserId = currentUserToken.uid
 
-  console.log(`Previous user ID: ${previousUserId}`)
-  console.log(`New user ID: ${currentUserId}`)
+  logger.log(`Migrating tasks from previous userID [${previousUserId}] to new userID [${currentUserId}].`)
 
-  console.log(`Migrating tasks from old userID [${previousUserId}] to new userID [${currentUserId}].`)
+  return db.runTransaction( async transaction => {
+    const tasksToMigrateQuery = db.collection('tasks').where('userId', '==', previousUserId)
+    const tasksToMigrate = await transaction.get(tasksToMigrateQuery)
 
-  const batch = db.batch()
-  const tasksToMigrate = await db.collection('tasks').where('userId', '==', previousUserId).get()
-  if (tasksToMigrate.empty) {
-    console.log(`Previous user [${previousUserId}] didn\'t have any documents, nothing to do.`)
+    if (tasksToMigrate.empty) {
+      logger.log(`Previous user [${previousUserId}] didn\'t have any documents, nothing to do.`)
+    }
+    else {
+      logger.log(`Migrating ${tasksToMigrate.size} tasks from userID [${previousUserId}] to new userId [${currentUserId}]`)
+      tasksToMigrate.forEach(snapshot => {
+        transaction.update(snapshot.ref, { 'userId': currentUserId })
+      })
+    }
+    return {
+      'updatedDocCount': tasksToMigrate.size,
+      'previousUserId': previousUserId,
+      'newUserId': currentUserId
+    }
+  })
+}
+
+export const migrateTasks = functions.https.onCall(async (data, context) => {
+  initialize()
+  
+  if (context.auth) {
+    logger.log('Received data: %j', data)
+
+    const idToken = data.idToken
+    logger.log(`Verifying ID token ${idToken}`)
+    try {
+      const decodedToken = await admin.auth().verifyIdToken(idToken)
+      return performMigration(decodedToken, context.auth?.token)        
+    } 
+    catch (error) {
+      logger.error(`Error when trying to verify ID token. Error: ${error}`)  
+      return { error }
+    }
   }
   else {
-    console.log(`Migrating ${tasksToMigrate.size} tasks from userID [${previousUserId}] to new userId [${currentUserId}]`)
-    tasksToMigrate.forEach(snapshot => {
-      batch.update(snapshot.ref, { 'userId': currentUserId })
-    })
-  
-    await batch.commit()
+    logger.error(`Unauthorized call.`)
+    return { }
   }
-
-  return {
-    'updatedDocCount': tasksToMigrate.size,
-    'previousUserId': previousUserId,
-    'newUserId': currentUserId
-  }
-}
+})
