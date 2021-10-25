2254a031a8b15a6a40b1eb7a41137dab70c8f7ca
Peter Friese <peter@peterfriese.de>
2021-10-25 23:36:37 +0200

    🎉 Let there be code!
    Signed-off-by: Peter Friese <peter@peterfriese.de>

---
+++ code/frontend/MakeItSo/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Peter Friese on 25.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ code/frontend/MakeItSo/Shared/MakeItSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItSoApp.swift
+//  Shared
+//
+//  Created by Peter Friese on 25.10.21.
+//
+
+import SwiftUI
+
+@main
+struct MakeItSoApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
