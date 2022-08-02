fba781c057ed4bb3e61d58f07a3209bb18d4bc60
Peter Friese <peter@peterfriese.de>
2022-08-02 19:42:23 +0200

    🚧 Work in progress

---
+++ code/frontend/MakeItSo/MakeItSo/App/MakeItSoApp.swift
@@ -0,0 +1,46 @@
+//
+//  MakeItSoApp.swift
+//  MakeItSo
+//
+//  Created by Peter Friese on 26.07.22.
+//  Copyright © 2022 Google LLC. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+import Resolver
+import FirebaseCore
+import AuthenticationServices
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  @LazyInjected var authenticationService: AuthenticationService
+  
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    authenticationService.signIn()
+    return true
+  }
+}
+
+@main
+struct MakeItSoApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  @LazyInjected var authenticationService: AuthenticationService
+  
+  var body: some Scene {
+    WindowGroup {
+      TodosListView()
+    }
+  }
+}
---
+++ code/frontend/MakeItSo/MakeItSo/Features/Authentication/ViewModels/AuthenticationViewModel.swift
@@ -0,0 +1,194 @@
+//
+//  AuthenticationViewModel.swift
+//  Favourites
+//
+//  Created by Peter Friese on 08.07.2022
+//  Copyright © 2021 Google LLC. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+import Resolver
+import FirebaseAuth
+
+// For Sign in with Apple
+import AuthenticationServices
+import CryptoKit
+
+enum AuthenticationState {
+  case unauthenticated
+  case authenticating
+  case authenticated
+}
+
+enum AuthenticationFlow {
+  case login
+  case signUp
+}
+
+@MainActor
+class AuthenticationViewModel: ObservableObject {
+  @LazyInjected private var authenticationService: AuthenticationService
+  
+  @Published var email: String = ""
+  @Published var password: String = ""
+  @Published var confirmPassword: String = ""
+
+  @Published var flow: AuthenticationFlow = .login
+
+  @Published var isValid: Bool  = false
+  @Published var authenticationState: AuthenticationState = .unauthenticated
+  @Published var errorMessage: String = ""
+  
+  @Published var user: User?
+  @Published var displayName: String = ""
+
+  private var currentNonce: String?
+
+  init() {
+    authenticationService
+      .$user
+      .assign(to: &$user)
+    
+    authenticationService
+      .$displayName
+      .assign(to: &$displayName)
+    
+    authenticationService
+      .$authenticationState
+      .assign(to: &$authenticationState)
+    
+    $flow
+      .combineLatest($email, $password, $confirmPassword)
+      .map { flow, email, password, confirmPassword in
+        flow == .login
+        ? !(email.isEmpty || password.isEmpty)
+        : !(email.isEmpty || password.isEmpty || confirmPassword.isEmpty)
+      }
+      .assign(to: &$isValid)
+  }
+
+  func switchFlow() {
+    flow = flow == .login ? .signUp : .login
+    errorMessage = ""
+  }
+
+  func reset() {
+    flow = .login
+    email = ""
+    password = ""
+    confirmPassword = ""
+  }
+}
+
+// MARK: - Sign in with Email and Password
+
+extension AuthenticationViewModel {
+  func signInWithEmailPassword() async -> Bool {
+    authenticationState = .authenticating
+    do {
+      try await authenticationService.signIn(withEmail: email, password: password)
+      return true
+    }
+    catch  {
+      print(error)
+      errorMessage = error.localizedDescription
+      authenticationState = .unauthenticated
+      return false
+    }
+  }
+
+  func signUpWithEmailPassword() async -> Bool {
+    authenticationState = .authenticating
+    do  {
+//      try await authenticationService.signUp(withEmail: email, password: password)
+      try await authenticationService.link(withEmail: email, password: password)
+      return true
+    }
+    catch {
+      print(error)
+      errorMessage = error.localizedDescription
+      authenticationState = .unauthenticated
+      return false
+    }
+  }
+
+  func signOut() {
+    do {
+      try authenticationService.signOut()
+    }
+    catch {
+      print(error)
+      errorMessage = error.localizedDescription
+    }
+  }
+
+  func deleteAccount() async -> Bool {
+    do {
+      try await authenticationService.deleteAccount()
+      return true
+    }
+    catch {
+      errorMessage = error.localizedDescription
+      return false
+    }
+  }
+}
+
+// MARK: - Sign in with Apple
+
+extension AuthenticationViewModel {
+
+  func handleSignInWithAppleRequest(_ request: ASAuthorizationAppleIDRequest) {
+    request.requestedScopes = [.fullName, .email]
+    let nonce = randomNonceString()
+    currentNonce = nonce
+    request.nonce = sha256(nonce)
+  }
+
+  func handleSignInWithAppleCompletion(_ result: Result<ASAuthorization, Error>) {
+    if case .failure(let failure) = result {
+      errorMessage = failure.localizedDescription
+    }
+    else if case .success(let authorization) = result {
+      if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+        guard let nonce = currentNonce else {
+          fatalError("Invalid state: a login callback was received, but no login request was sent.")
+        }
+        guard let appleIDToken = appleIDCredential.identityToken else {
+          print("Unable to fetdch identify token.")
+          return
+        }
+        guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+          print("Unable to serialise token string from data: \(appleIDToken.debugDescription)")
+          return
+        }
+
+        let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                  idToken: idTokenString,
+                                                  rawNonce: nonce)
+        Task {
+          do {
+            let result = try await Auth.auth().signIn(with: credential)
+            try await result.user.updateDisplayName(with: appleIDCredential)
+            displayName = result.user.displayName ?? ""
+          }
+          catch {
+            print("Error: \(error.localizedDescription)")
+          }
+        }
+      }
+    }
+  }
+
+}
