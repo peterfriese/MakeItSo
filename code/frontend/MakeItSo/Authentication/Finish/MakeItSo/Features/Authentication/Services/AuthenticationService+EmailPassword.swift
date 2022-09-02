0aa7a6167efcbe7c1998855aa2f8f70fedb012bd
Marina Coelho <marinacoelho16@gmail.com>
2022-09-02 14:56:27 +0100

    Add start and finish folders for Authentication

---
+++ code/frontend/MakeItSo/Authentication/Finish/MakeItSo/Features/Authentication/Services/AuthenticationService+EmailPassword.swift
@@ -0,0 +1,59 @@
+//
+//  AuthenticationService+EmailPassword.swift
+//  MakeItSo
+//
+//  Created by Peter Friese on 29.08.22.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+import FirebaseAuth
+
+// MARK: - Sign in with Email and Password
+
+extension AuthenticationService {
+  
+  /// Signs in using an email address and a password.
+  /// - Parameters:
+  ///   - email: The user's email
+  ///   - password: The user's password
+  func signIn(withEmail email: String, password: String) async throws {
+    try await Auth.auth().signIn(withEmail: email, password: password)
+  }
+
+  /// Signs up using an email address and a password. If the current user is an anonymous user,
+  /// upgrades the anonymous user to a full account by linking it using the given email and password.
+  /// - Parameters:
+  ///   - email: The user's email
+  ///   - password: The user's password
+  func signUp(withEmail email: String, password: String) async throws {
+    if let user, user.isAnonymous {
+      try await link(withEmail: email, password: password)
+    }
+    else {
+      try await Auth.auth().createUser(withEmail: email, password: password)
+    }
+  }
+  
+  /// Link email and password credentials to an existing user.
+  /// - Parameters:
+  ///   - email: The user's email
+  ///   - password: The user's password
+  func link(withEmail email: String, password: String) async throws {
+    let credential = EmailAuthProvider.credential(withEmail: email, password: password)
+    let result = try await user?.link(with: credential)
+    await MainActor.run {
+      self.user = result?.user
+    }
+  }
+}
---
+++ code/frontend/MakeItSo/Authentication/Start/MakeItSo/Features/Authentication/Services/AuthenticationService.swift
@@ -0,0 +1,94 @@
+//
+//  AuthenticationService.swift
+//  MakeItSo
+//
+//  Created by Peter Friese on 26.07.22.
+//  Copyright © 2022 Google LLC. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import Foundation
+import os
+import Resolver
+import FirebaseCore
+import FirebaseAuth
+import Combine
+
+public class AuthenticationService: ObservableObject {
+  private let logger = Logger(subsystem: "com.google.firebase.quickstart.MakeItSo", category: "authentication")
+  
+  @Published var user: User?
+  @Published var displayName: String = ""
+  @Published var authenticationState: AuthenticationState = .unauthenticated
+  
+  private var authStateHandler: AuthStateDidChangeListenerHandle?
+  internal var currentNonce: String?
+  
+  private var cancellables = Set<AnyCancellable>()
+  
+  init() {
+    registerAuthStateHandler()
+    verifySignInWithAppleAuthenticationState()
+    registerCredentialRevocationListener()
+    
+    $user
+      .map { user in
+        user?.displayName ?? user?.email ?? ""
+      }
+      .assign(to: &$displayName)
+    
+    $user
+      .sink { user in
+        if user == nil {
+          self.logger.debug("User signed out.")
+          // sign in anonymously
+          self.signIn()
+        }
+      }
+      .store(in: &cancellables)
+    
+    $user
+      .compactMap { $0 }
+      .map { user in
+        user.isAnonymous
+          ? AuthenticationState.unauthenticated
+          : .authenticated
+      }
+      .assign(to: &$authenticationState)
+  }
+  
+  private func registerAuthStateHandler() {
+    if authStateHandler == nil {
+      authStateHandler = Auth.auth().addStateDidChangeListener { auth, user in
+        self.user = user
+      }
+    }
+  }
+  
+}
+
+// MARK: - Generic account operations
+extension AuthenticationService {
+  
+  /// If no user is signed in, sign in anonymously
+  func signIn() {
+
+  }
+  
+  func signOut() throws {
+    try Auth.auth().signOut()
+  }
+  
+  func deleteAccount() async throws {
+    try await user?.delete()
+  }
+}
