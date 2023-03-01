88fb80775ee0f263699f3dd7e2a81bfcec387e9a
Peter Friese <peter@peterfriese.de>
2023-03-01 19:04:22 +0100

    ✨ Simple todo list app with in-memory storage
    Signed-off-by: Peter Friese <peter@peterfriese.de>

---
+++ code/frontend/MakeItSo/MakeItSo/Features/Reminders/Views/RemindersListView.swift
@@ -0,0 +1,71 @@
+//
+// ContentView.swift
+// MakeItSo
+//
+// Created by Peter Friese on 01.03.23.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+
+struct RemindersListView: View {
+  @State
+  private var reminders = Reminder.samples
+
+  @State
+  private var isAddReminderDialogPresented = false
+
+  private func presentAddReminderView() {
+    isAddReminderDialogPresented.toggle()
+  }
+
+  var body: some View {
+    List($reminders) { $reminder in
+      HStack {
+        Image(systemName: reminder.completed
+              ? "largecircle.fill.circle"
+              : "circle")
+          .foregroundColor(.accentColor)
+          .onTapGesture {
+            reminder.completed.toggle()
+          }
+        Text(reminder.title)
+      }
+    }
+    .toolbar {
+      ToolbarItemGroup(placement: .bottomBar) {
+        Button(action: presentAddReminderView) {
+          HStack {
+            Image(systemName: "plus.circle.fill")
+            Text("New Reminder")
+          }
+        }
+        Spacer()
+      }
+    }
+    .sheet(isPresented: $isAddReminderDialogPresented) {
+      AddReminderView { reminder in
+        reminders.append(reminder)
+      }
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationStack {
+      RemindersListView()
+        .navigationTitle("Reminders")
+    }
+  }
+}
