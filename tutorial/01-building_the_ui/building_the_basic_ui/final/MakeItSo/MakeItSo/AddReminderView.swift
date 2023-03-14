0f438db838fc977df96a912a4a86de95ee25fe9e
Peter Friese <peter@peterfriese.de>
2023-03-14 08:30:14 +0100

    📦️ Update tutorial files
    Signed-off-by: Peter Friese <peter@peterfriese.de>

--- tutorial/01-building_the_ui/building_the_basic_ui/final/MakeItSo/MakeItSo/AddReminderView.swift
+++ tutorial/01-building_the_ui/building_the_basic_ui/final/MakeItSo/MakeItSo/AddReminderView.swift
@@ -32,10 +32,10 @@ struct AddReminderView: View {
   @Environment(\.dismiss)
   private var dismiss
 
-  var completion: (_ reminder: Reminder) -> Void
+  let onCommit: (_ reminder: Reminder) -> Void
 
   private func commit() {
-    completion(reminder)
+    onCommit(reminder)
     dismiss()
   }
 
